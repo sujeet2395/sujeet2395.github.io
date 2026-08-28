b112702987028787e5fe982b205445026548b7ab
Sujit Kumar <sujeet.sharma2395@gmail.com>
2026-08-29 00:19:21 +0530

    dist directory to build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sujeet2395.github.io/',
+  base: '/',
+  build: {
+    outDir: 'build',
+  },
 });
